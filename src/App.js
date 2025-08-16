b1c54b07e1039f797814ba29f9c87bbed2925f26
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:31 +0530

    Initial commit: News-Reader

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "News-Reader"</h1>
+        <p>"Content aggregation app"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
